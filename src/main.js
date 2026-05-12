0201665e675f3aadb97b57b58bad0d74986b7221
Miguel-Seco <miguelseco31@gmail.com>
2026-05-12 20:10:16 -0300

    prueba de que no pase mi error al tp

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ const commentsRouter = require('./routes/comments.route');
 const userRouter = require('./routes/user.route');
 const postRouter = require('./routes/post.route');
 
+const ayuda = "ojala pueda escribir"
+
 app.use(express.json());
 app.use('/comments', commentsRouter);
 app.use('/user', userRouter);
@@ -20,3 +22,7 @@ app.listen(PORT, async (err) =>{
     await db.sync();
     console.log(`Server is running on port ${PORT}`);
 })
+
+// prueba push 
+
+
