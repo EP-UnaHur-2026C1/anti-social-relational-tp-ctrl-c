933fa3fa806baf8d97a8fd2464dfa6f608522d94
Bruno Tundis <brunotundis34@gmail.com>
2026-05-15 17:24:32 -0300

    validacion de schema en tag y comments

--- src/routes/comments.route.js
+++ src/routes/comments.route.js
@@ -3,10 +3,13 @@ const router = Router();
 const middleware = require('../middlewares/validaciones.middleware');
 const {Comment} = require('../db/models');
 const {getComments, getCommentById, createComment, updateComment, deleteComment} = require('../controllers/comments.controller');
+const schemaValidator = require('../middlewares/schemaValidator.middleware')
+const userSchema = require('../schemas/user.schema')
+
 
 router.get('/',getComments)
 router.get('/:id',middleware.validaIdNumerico,middleware.validaExisteMiddleware(Comment), getCommentById)
-router.post('/create', createComment)
+router.post('/create',schemaValidator(userSchema), createComment)
 router.put('/:id',middleware.validaIdNumerico ,updateComment)
 router.delete('/:id',middleware.validaIdNumerico,middleware.validaExisteMiddleware ,deleteComment)
 
--- src/routes/tag.route.js
+++ src/routes/tag.route.js
@@ -3,11 +3,12 @@ const router = Router();
 const {getTags, getTagById, createTag,updateTag, deleteTag} = require('../controllers/tag.controller')
 const middleware = require('../middlewares/validaciones.middleware')
 const {Tag} = require('../db/models')
-
+const schemaValidator = require('../middlewares/schemaValidator.middleware')
+const userSchema = require('../schemas/user.schema')
 
 router.get('/', getTags)
 router.get('/:id',middleware.validaIdNumerico,middleware.validaExisteMiddleware(Tag), getTagById)
-router.post('/create', createTag)
+router.post('/create',schemaValidator(userSchema), createTag)
 router.put('/:id',middleware.validaIdNumerico,middleware.validaExisteMiddleware(Tag), updateTag)
 router.delete('/:id',middleware.validaIdNumerico,middleware.validaExisteMiddleware(Tag), deleteTag)
 
