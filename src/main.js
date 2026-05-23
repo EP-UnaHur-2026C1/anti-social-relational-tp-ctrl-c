ee0c0f219d93759a80ac5c230ed50b19457fba32
NicolasUrdiales <nicolas.urdiales@estudiantes.unahur.edu.ar>
2026-05-23 15:09:11 -0300

    documentacion de la api con swagger

--- src/main.js
+++ src/main.js
@@ -8,9 +8,16 @@ const postRouter = require('./routes/post.route');
 const postImageRouter = require('./routes/post_image.route');
 const tagRouter = require('./routes/tag.route');
 
+// ----------------- Swagger -------------------
 
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+const swaggerDocument = YAML.load(path.join(__dirname, '../docs/swagger.yaml'));
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use('/comments', commentsRouter);
 app.use('/users', userRouter);
 app.use('/posts', postRouter);
